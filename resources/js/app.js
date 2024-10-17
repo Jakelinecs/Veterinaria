5a5cb36d15d4bad7e4c3c1c52e36cba860754616
Jakelinecs <jakeli1997.jcs@gmail.com>
2024-10-17 18:40:08 -0400

    initio

---
+++ resources/js/app.js
@@ -0,0 +1,27 @@
+import './bootstrap';
+require('./alpinejs');
+
+// TailwindCSS Dark Mode
+
+// recupera el modo desde localStorage cuando la pagina carga la primera vez
+if (localStorage.theme === 'dark' || (!'theme' in localStorage && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.querySelector('html').classList.add('dark')
+} else if (localStorage.theme === 'dark') {
+    document.querySelector('html').classList.add('dark')
+}
+
+// Evento click de los botones.
+// Agreag la clase 'dark' al elemento html
+// guarda o elimina el modo del localstorage
+document.querySelectorAll(".setMode").forEach(item =>
+    item.addEventListener("click", () => {
+        let htmlClasses = document.querySelector('html').classList;
+        if(localStorage.theme == 'dark') {
+            htmlClasses.remove('dark');
+            localStorage.theme = ''
+        } else {
+            htmlClasses.add('dark');
+            localStorage.theme = 'dark';
+        }
+    })
+)
